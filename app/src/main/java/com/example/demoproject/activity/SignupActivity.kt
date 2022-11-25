4d4a7ab70336ad233c4b9c63ec93ce00ee1be923
shubhamchauhan31 <shubhamchauhan97@gmail.com>
2022-11-25 10:19:16 +0530

    sign up screen create

---
+++ app/src/main/java/com/example/demoproject/activity/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.example.demoproject.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.demoproject.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
